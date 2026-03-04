7b17878e6ba49336547c322c0b55e5a09c4e0939
trung <trungvt138@github.com>
2026-03-04 01:16:24 +0100

    test passed

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-  preset: 'jest-expo',
-  transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
-  ],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': ['babel-jest', {
+      configFile: false,
+      presets: [
+        ['@babel/preset-typescript'],
+      ],
+      plugins: [
+        ['@babel/plugin-transform-modules-commonjs'],
+      ],
+    }],
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   testMatch: ['**/__tests__/**/*.test.(ts|tsx|js)'],
-  setupFilesAfterFramework: ['@testing-library/jest-native/extend-expect'],
 };
